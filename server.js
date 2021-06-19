169cfb2356aa5c8c51794dc6e44cea8b530a631e
Maria T. Andersen <mteilmann93@gmail.com>
2021-06-19 18:42:43 +0200

    create public folder with 3 files, index.html, main.js and style.css. Edit code in serve.js file

--- server.js
+++ server.js
@@ -1,11 +1,41 @@
 const express = require("express");
+const socket = require("socket.io");
+
+// App setup
+const port = 3000;
 const app = express();
 const server = require("http").createServer(app);
 
+// Static files
+app.use(express.static("public"));
+
+// socket setup
+const io = socket(server);
+
 app.get("/", function (req, res) {
-  res.send("Hello World");
+  res.sendFile("index.html");
 });
 
-server.listen(3000, () => {
-  console.log("Server listening on Port 3000...");
+const activeUsers = new Set();
+
+// // Whenever someone connects this gets executed
+// io.on("connection", function (socket) {
+//   console.log("A user connected");
+
+//   socket.on("new user", function (data) {
+//     socket.userId = data;
+//     activeUsers.add(data);
+//     io.emit("new user", [...activeUsers]);
+//   });
+
+//   //Whenever someone disconnects this piece of code gets executed
+//   socket.on("disconnect", () => {
+//     activeUsers.delete(socket.userId);
+//     io.emit("user disconnected", socket.userId);
+//   });
+// });
+
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+  console.log(`http://localhost:${port}`);
 });
