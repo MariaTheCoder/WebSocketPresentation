ad0d68cca1e81af3c2ec4bf5e5e36a32e835fea0
Maria T. Andersen <mteilmann93@gmail.com>
2021-06-22 20:13:58 +0200

    rename main.js to clients.js and add socket connection. Make the socket listen for reset requests from the presenter

---
+++ public/clients.js
@@ -0,0 +1,26 @@
+// Make socket connection back-end
+const socket = io();
+
+const activeUsers = document.getElementById("active_users");
+
+// Emit events
+document.getElementById("green_btn").addEventListener("click", function () {
+  socket.emit("status", {
+    id: socket.id,
+    finished: true,
+    help: false,
+  });
+});
+
+// Emit events
+document.getElementById("red_btn").addEventListener("click", function () {
+  socket.emit("status", {
+    id: socket.id,
+    finished: false,
+    help: true,
+  });
+});
+
+socket.on("reset", () => {
+  console.log("The Dictator has resetted everything!");
+});
